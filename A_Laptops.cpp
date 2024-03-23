6570841e736853e7ea4a7984f449ed1d665bd6fb
Sanchit Vijay <71404343+sanchiitvijay@users.noreply.github.com>
2024-03-24 04:11:00 +0530

    23-03-24

--- A_Laptops.cpp
+++ A_Laptops.cpp
@@ -3,32 +3,20 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    map<int,pair<int, int>> v;
-    set<int> st;
-    int a,b;
-    for(int i = 0; i<n; i++) {
-        cin>>a>>b;
-        v[a] = {min(v[a].first, b),max(v[a].second, b)};
-        st.insert(a);
+    pair<int,int> pr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>pr[i].first>>pr[i].second;
     }
-    vector<int> ve;
-    for(auto i : st) {
-        ve.push_back(i);
-    }
-    sort(ve.begin(), ve.end());
-    // for(auto i: ve) {
-    //     cout<<i<<" "<<v[i].first<<" "<<v[i].second<<endl;
-    // }
-
-    for(int i = 0; i<st.size()-1; i++) {
-        for(int j = i+1; j<st.size(); j++) {
-            if((v[ve[j]].first == 0 && v[ve[i]].second > v[ve[j]].second) || (v[ve[j]].first != 0 && v[ve[i]].second > v[ve[j]].first)) {
-                cout<<"Happy Alex"<<endl;
-                return 0;
-            }
+    sort(pr,pr+n);
+    for(int j=0;j<n-1;j++)
+    {
+        if(pr[j].first<pr[j+1].first && pr[j].second>pr[j+1].second)
+        {
+            cout<<"Happy Alex"<<endl;
+            return 0;
         }
     }
     cout<<"Poor Alex"<<endl;
-    
     return 0;
 }
